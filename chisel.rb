cfb27996eef080ba18958c7c316031d35ae2930b
Tyler Graham <Tyler@Tylers-MacBook-Pro.local>
2015-02-20 15:53:13 -0700

    all sub-methods, no complete method

--- chisel.rb
+++ chisel.rb
@@ -2,12 +2,12 @@ class Chisel
 
 #   def initialize
 # @words1 =
-# '# My Life in Desserts
-#
-# ## Chapter 1: The Beginning
-#
-# "You just *have* to try the cheesecake," he said. "Ever since it appeared in **Food & Wine** this place has been packed every night."'
-#   end
+'# My Life in Desserts
+
+## Chapter 1: The Beginning
+
+"You just *have* to try the cheesecake," he said. "Ever since it appeared in **Food & Wine** this place has been packed every night."'
+#end
 
   def parse_header_one(words)
     #if words[0..1] == "# "
@@ -118,44 +118,47 @@ class Chisel
   words3.join(" ")
   end
 
+
+  #input = "# My Life in Desserts\n\n## Chapter 1:
+  #        The Beginning\n\n\"You just *have* to try the
+  #        cheesecake,\" he said. \"Ever since it appeared
+  #        in **Food & Wine** this place has been packed
+  #        every night.\""
+  # def parse_all(words1)
+  #    words1.split("\n\n").map do |i|
+  #      if i[0] == "#"
+  #        i.parse_header(i)
+  #      else
+  #        i.
+  #
+  #      if new_line == false && in_a_tag == false && i == "#"
+  #        i.sub(i, "<h1>")
+  #         #after hit \n flip new_line to true
+  #         #after hit \n\n flip new_paragraph to true
+  #       end
+  #
+  #
+  #      if in_a_tag == false && i == "*"
+  #        in_a_tag = true
+  #        i.sub(i, "<em>")
+  #      elsif in_a_tag == true && i == "*"
+  #        in_a_tag = false
+  #        i.sub(i, "</em>")
+  #      else
+  #        i
+  #      end
+  #    end
+  #    text1.join
+  # end
+
+end
+
 # can use count on # to determine what level of header
 # .scan( /#+\s(.+)/ )
 # .scan( /#[what string starts with]+[means one or more,
 # * means zero or more]\s[space](.+)[capture 1+ periods] / )
 # note: \w will grab any words; scan always gives array,
 # can go .first.first to change array back to original string
-
-  def parse_all(words1)
-    new_line = false
-    new_paragraph = true
-
-    in_a_title = false
-    in_a_paragraph = false
-    in_a_tag = false
-
-     words1.split("\n\n").map do |i|
-       if new_line == false && in_a_tag == false && i == "#"
-         i.sub(i, "<h1>")
-          #after hit \n flip new_line to true
-          #after hit \n\n flip new_paragraph to true
-        end
-
-
-       if in_a_tag == false && i == "*"
-         in_a_tag = true
-         i.sub(i, "<em>")
-       elsif in_a_tag == true && i == "*"
-         in_a_tag = false
-         i.sub(i, "</em>")
-       else
-         i
-       end
-     end
-     text1.join
-  end
-
-end
-
 
 # use \n\n to separate lines in the output
 # just add <p> + to beginning of string and + </p> to the end
