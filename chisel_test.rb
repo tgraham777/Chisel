85c7bc2564e745dc3ce8a5ddc6819f2ad6f4c56b
Tyler Graham <Tyler@Tylers-MacBook-Pro.local>
2015-02-20 09:39:50 -0700

    adds first tests

---
+++ chisel_test.rb
@@ -0,0 +1,73 @@
+require "minitest"
+require "minitest/autorun"
+require_relative "chisel.rb"
+
+class ChiselTest < Minitest::Test
+
+  def test_header_one
+    chisel = Chisel.new
+    parser = chisel.parse_header_one("# This is Awesome")
+    assert_equal "<h1>This is Awesome</h1>", parser
+  end
+
+  def test_header_two
+    chisel = Chisel.new
+    parser = chisel.parse_header_two("## This is Awesome")
+    assert_equal "<h2>This is Awesome</h2>", parser
+  end
+
+  def test_header_three
+    chisel = Chisel.new
+    parser = chisel.parse_header_three("### This is Awesome")
+    assert_equal "<h3>This is Awesome</h3>", parser
+  end
+
+  def test_header_four
+    chisel = Chisel.new
+    parser = chisel.parse_header_four("#### This is Awesome")
+    assert_equal "<h4>This is Awesome</h4>", parser
+  end
+
+  def test_header_five
+    chisel = Chisel.new
+    parser = chisel.parse_header_five("##### This is Awesome")
+    assert_equal "<h5>This is Awesome</h5>", parser
+  end
+
+  def test_paragraph
+    chisel = Chisel.new
+    parser = chisel.parse_paragraph("This is such an awesome paragraph.")
+    assert_equal "<p>\nThis is such an awesome paragraph.\n</p>", parser
+  end
+
+  def test_parse_all
+    skip
+  end
+
+end
+
+
+#def test_it_exists
+#  parser = Chisel.new
+#end
+
+
+# def test_emphasis_1
+#   chisel = Chisel.new
+#   parser = chisel.parse_emphasis("*hello* *my* *friend*")
+#   assert_equal "<em>hello</em> <em>my</em> <em>friend</em>", parser # my <em>friend</em>", parser
+# end
+#
+# def test_emphasis_strong
+#   chisel = Chisel.new
+#   parser = chisel.parse_strong_emphasis("**hello** my **friend**")
+#   assert_equal "<strong>hello</strong> my <strong>friend</strong>", parser # my <em>friend</em>", parser
+# end
+#
+# def test_emphasis_strong
+#
+# end
+#
+# def test_emphasis_strong
+#
+# end
